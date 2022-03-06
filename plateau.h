0db1def62e60536b1dc2401ae85870cf602e043d
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-03-06 17:03:35 +0100

    Tâche A.3 plateau.h version 4

--- plateau.h
+++ plateau.h
@@ -12,7 +12,7 @@
 
 /* implémentation du type plateau */
 typedef struct plateau {
-    int tab[1000][1000]; // matrice de taille 1000x1000
+    carte tab[1000][1000]; // matrice de cartes de taille 1000x1000
 } plateau;
 
 /*  droite  :   plateau[i+1][j]
@@ -28,27 +28,27 @@ plateau init_plateau();
 /*  @requires   un plateau p valide
     @assigns    libère la mémoire associée au plateau
     @ensures    rien */
-void free_plateau(plateau p);
+void libere_plateau(plateau p);
 
 /*  @requires   un plateau p valide
     @assigns    rien
     @ensures    renvoie 1 si il initialise une nouvelle manche de jeu
                 renvoie 0 si le jeu est terminé */
-int end_round(plateau p);    
+int fin_manche(plateau p);    
 
 /*  @requires   un plateau p valide
     @assigns    rien
     @ensures    renvoie la liste des deux factions du jeu */
-list list_faction(plateau p);
+liste_faction liste_faction(plateau p);
 
 /*  @requires   un plateau p et une carte c valides
     @assigns    pose la carte c face cachée sur le plateau p
     @ensures    rien */
-void put(plateau p, carte c);
+void poser(plateau p, carte c);
 
 /*  @requires   un plateau p et une carte c valides
     @assigns    retourne la carte c face visible et active son effet
     @ensures    rien */
-void turn(plateau p, carte c);
+void retourner(plateau p, carte c);
 
 #endif

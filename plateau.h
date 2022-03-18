bfd790d04b42f1b1fb9aeafd77c8d887b87a9cf0
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-03-18 15:08:37 +0100

    Tâche A.3 - plateau.h - version 8

--- plateau.h
+++ plateau.h
@@ -8,7 +8,7 @@
 #include "faction.h"
 
 /* implémentation du type abstrait plateau */
-typedef struct plateau plateau;
+typedef struct plateau *plateau;
 
 /*  @requires   rien
     @assigns    rien
@@ -26,20 +26,20 @@ void libere_plateau(plateau p);
                 renvoie 0 si le jeu est terminé */
 int fin_manche(plateau p);
 
-/*  @requires   un plateau p valide
+/*  @requires   rien
     @assigns    rien
-    @ensures    renvoie la liste des deux factions du jeu sous forme de tableau */
-faction* liste_faction(plateau p);
+    @ensures    initialise la liste avec deux factions en jeu */
+faction* liste_faction();
 
 /*  @requires   un plateau p valide, une carte c valide et un tableau pos = [x, y] contenant les deux coordonnées de la position où on pose la carte
     @assigns    ajoute la carte c à la case en position x et y de plateau p
     @ensures    pose la carte c face cachée sur le plateau p */
 void poser(plateau p, carte c, int* pos);
 
-/*  @requires   un plateau p valide
+/*  @requires   un plateau p valide et une liste des factions en jeu
     @assigns    change l'état de la carte la plus en haut à gauche du plateau p
     @ensures    retourne la carte la plus en haut à gauche face visible et active son effet
                 renvoie la carte retournée ou NULL si toutes les cartes du plateau sont déjà face visible */
-carte retourner(plateau p);
+carte retourner(plateau p, faction* facs);
 
 #endif

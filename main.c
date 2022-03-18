49c915942843322836c451b12a53f0dd40f2cefd
Willy KINFOUSSIA <willy.kinfoussia@pc130-25.pedago.ensiie.fr>
2022-03-18 15:43:33 +0100

    Tache A.1 main.c commenté

--- main.c
+++ main.c
@@ -10,33 +10,52 @@
 /* importation du module plateau */
 #include "plateau.h"
 
+/* Pour le null */
 #include <stddef.h>
 
-
 int main(){
+
+    // Initialisation du plateau
     plateau p = init_plateau();
+
+    // Initialisation de la liste des factions
     faction* facs = liste_faction();
+
+    // Affichage du plateau sur la console
     affiche_plateau(p);
 
-    // Manche
+    // Initialisation d'une manche si le jeu n'est pas terminé 
     while(fin_manche(p) != 0){
-        // Init joueur
+        // Initialisation des joueurs
         for(int i = 0; i < 2; i++){
+            // Affichage la main de la faction i dans la console
             affiche_main(facs[i]);
+            // Demande à la faction actuelle si elle veut utiliser son option permettant de repiocher
             decision();
+            // Renvoie 1 si l'option remélanger a été utilisée, 0 autrement 
             if(test_remelanger(facs[i]))
+            // Remelange les cartes de la fonction
                 remelanger(facs[i]);
         }
 
-        // Phase 1
+        // Phase 1 :
+
+        // Boucle pour chaque carte qu'on va poser
         for(int i = 0; i < 16; i++){
+            // Affiche la main d'une faction tour par tour
             affiche_main(facs[i%2]);
+            // Retourne la carte choisi par la faction
             carte c = carte_choisie(facs[i%2]);
+            // Retourne la position sur le plateau où la faction décide de poser la carte
             int* pos = carte_positon(p);
+            // Pose la carte sur le plateau
             poser(p, c, pos);
         }
 
-        // Phase 2
+        // Phase 2 :
+
+        // Boucle infini on s'arrete quand il n'y a plus de carte sur le plateau
+        // Retourne la carte la plus haute à gauche si elle existe et affiche son effet
         while(1){
             carte c = retourner(p, facs);
             if(c == NULL)
@@ -45,6 +64,8 @@ int main(){
         }
     }
 
+    // Affichage du gagnant
     gagnant(facs);
+
     return 0;
 }

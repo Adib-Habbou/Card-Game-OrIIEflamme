5d3fec75417702e6d04e4ca2a53e91f08036955f
Willy KINFOUSSIA <willy.kinfoussia@pc130-25.pedago.ensiie.fr>
2022-03-18 16:58:54 +0100

    Tache B.6 initialilsatoin faction.c et plateau.c

---
+++ src/faction.c
@@ -0,0 +1,25 @@
+#include "faction.h"
+
+
+
+
+
+int test_remelanger(faction fact){ 
+    
+}
+
+void remelanger(faction fact){
+
+}
+
+void vider_main(faction fact){
+
+}
+
+void melanger_pioche(faction fact){
+
+}
+
+void repiocher(faction fact){
+
+}
---
+++ src/plateau.c
@@ -0,0 +1,26 @@
+#include "plateau.h"
+
+
+plateau init_plateau(){
+
+}
+
+void libere_plateau(plateau p){
+
+}
+
+int fin_manche(plateau p){
+
+}
+
+faction* liste_faction(){
+
+}
+
+void poser(plateau p, carte c, int* pos){
+
+}
+
+carte retourner(plateau p, faction* facs){
+
+}

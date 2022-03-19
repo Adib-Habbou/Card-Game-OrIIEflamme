a5f6415c960b0de1e768afbb6c8e14baad81819b
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-03-19 16:26:10 +0100

    Tâche A.3 - plateau.h - version 9

--- plateau.h
+++ plateau.h
@@ -18,13 +18,13 @@ plateau init_plateau();
 /*  @requires   un plateau p valide
     @assigns    plateau p
     @ensures    libère la mémoire associée au plateau et à ses deux factions */
-void libere_plateau(plateau p);
+void libere_plateau(plateau plateau);
 
 /*  @requires   un plateau p valide
     @assigns    rien
     @ensures    renvoie 1 si il initialise une nouvelle manche de jeu
                 renvoie 0 si le jeu est terminé */
-int fin_manche(plateau p);
+int fin_manche(plateau plateau);
 
 /*  @requires   rien
     @assigns    rien
@@ -34,12 +34,12 @@ faction* liste_faction();
 /*  @requires   un plateau p valide, une carte c valide et un tableau pos = [x, y] contenant les deux coordonnées de la position où on pose la carte
     @assigns    ajoute la carte c à la case en position x et y de plateau p
     @ensures    pose la carte c face cachée sur le plateau p */
-void poser(plateau p, carte c, int* pos);
+void poser(plateau plateau, carte carte, int* position);
 
 /*  @requires   un plateau p valide et une liste des factions en jeu
     @assigns    change l'état de la carte la plus en haut à gauche du plateau p
     @ensures    retourne la carte la plus en haut à gauche face visible et active son effet
                 renvoie la carte retournée ou NULL si toutes les cartes du plateau sont déjà face visible */
-carte retourner(plateau p, faction* facs);
+carte retourner(plateau plateau, faction* liste_faction);
 
 #endif

65239934c61dde4b477a4ac03aefc50d77450b89
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-03-20 09:34:26 +0100

    Update de plateau.h suite à des changements sur la branche lot_a

--- headers/plateau.h
+++ headers/plateau.h
@@ -7,6 +7,9 @@
 /* importation du module faction */
 #include "faction.h"
 
+/* implémentation du type abstrait case utilisé pour implémenter plateau */
+typedef struct case *case;
+
 /* implémentation du type abstrait plateau */
 typedef struct plateau *plateau;
 
@@ -15,31 +18,31 @@ typedef struct plateau *plateau;
     @ensures    renvoie un nouveau plateau */
 plateau init_plateau();
 
-/*  @requires   un plateau p valide
-    @assigns    plateau p
+/*  @requires   un plateau valide
+    @assigns    plateau
     @ensures    libère la mémoire associée au plateau et à ses deux factions */
-void libere_plateau(plateau p);
+void libere_plateau(plateau plateau);
 
-/*  @requires   un plateau p valide
+/*  @requires   un plateau valide
     @assigns    rien
     @ensures    renvoie 1 si il initialise une nouvelle manche de jeu
                 renvoie 0 si le jeu est terminé */
-int fin_manche(plateau p);
+int init_manche(plateau plateau);
 
 /*  @requires   rien
     @assigns    rien
     @ensures    initialise la liste avec deux factions en jeu */
 faction* liste_faction();
 
-/*  @requires   un plateau p valide, une carte c valide et un tableau pos = [x, y] contenant les deux coordonnées de la position où on pose la carte
-    @assigns    ajoute la carte c à la case en position x et y de plateau p
-    @ensures    pose la carte c face cachée sur le plateau p */
-void poser(plateau p, carte c, int* pos);
+/*  @requires   un plateau, une carte valide et un tableau position = [x, y] contenant les deux coordonnées de la position où on pose la carte
+    @assigns    ajoute la carte à la case en position x et y du plateau
+    @ensures    pose la carte face cachée sur le plateau */
+void poser(plateau plateau, carte carte, int* position);
 
-/*  @requires   un plateau p valide et une liste des factions en jeu
-    @assigns    change l'état de la carte la plus en haut à gauche du plateau p
+/*  @requires   un plateau valide et une liste des factions en jeu
+    @assigns    change l'état de la carte la plus en haut à gauche du plateau
     @ensures    retourne la carte la plus en haut à gauche face visible et active son effet
                 renvoie la carte retournée ou NULL si toutes les cartes du plateau sont déjà face visible */
-carte retourner(plateau p, faction* facs);
+carte retourner(plateau plateau, faction* liste_faction);
 
 #endif

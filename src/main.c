5b0bd9c52ce43b97ff9ce4faf4a777123ec52595
Willy Kinfoussia <willy.kinfoussia@gmail.com>
2022-03-20 09:01:09 +0100

    Tache B.6 fonction init_plateau, libere_plateau de plateau.c

--- src/main.c
+++ src/main.c
@@ -16,26 +16,26 @@
 int main(){
 
     // Initialisation du plateau
-    plateau p = init_plateau();
+    plateau _plateau = init_plateau();
 
     // Initialisation de la liste des factions
-    faction* facs = liste_faction();
+    faction* _factions = liste_faction();
 
     // Affichage du plateau sur la console
-    affiche_plateau(p);
+    affiche_plateau(_plateau);
 
     // Initialisation d'une manche si le jeu n'est pas terminé 
-    while(fin_manche(p) != 0){
+    while(init_manche(_plateau) != 0){
         // Initialisation des joueurs
         for(int i = 0; i < 2; i++){
             // Affichage la main de la faction i dans la console
-            affiche_main(facs[i]);
+            affiche_main(_factions[i]);
             // Demande à la faction actuelle si elle veut utiliser son option permettant de repiocher
             decision();
             // Renvoie 1 si l'option remélanger a été utilisée, 0 autrement 
-            if(test_remelanger(facs[i]))
+            if(test_remelanger(_factions[i]))
             // Remelange les cartes de la fonction
-                remelanger(facs[i]);
+                remelanger(_factions[i]);
         }
 
         // Phase 1 :
@@ -43,13 +43,13 @@ int main(){
         // Boucle pour chaque carte qu'on va poser
         for(int i = 0; i < 16; i++){
             // Affiche la main d'une faction tour par tour
-            affiche_main(facs[i%2]);
+            affiche_main(_factions[i%2]);
             // Retourne la carte choisi par la faction
-            carte c = carte_choisie(facs[i%2]);
+            carte _carte = carte_choisie(_factions[i%2]);
             // Retourne la position sur le plateau où la faction décide de poser la carte
-            int* pos = carte_positon(p);
+            int* _position = carte_positon(_plateau);
             // Pose la carte sur le plateau
-            poser(p, c, pos);
+            poser(_plateau, _carte, _position);
         }
 
         // Phase 2 :
@@ -57,15 +57,15 @@ int main(){
         // Boucle infini on s'arrete quand il n'y a plus de carte sur le plateau
         // Retourne la carte la plus haute à gauche si elle existe et affiche son effet
         while(1){
-            carte c = retourner(p, facs);
-            if(c == NULL)
+            carte _carte = retourner(_plateau, _factions);
+            if(_carte == NULL)
                 break;
-            afficher_effet(c);
+            afficher_effet(_carte);
         }
     }
 
     // Affichage du gagnant
-    gagnant(facs);
+    gagnant(_factions);
 
     return 0;
 }
--- src/plateau.c
+++ src/plateau.c
@@ -1,7 +1,9 @@
 #include "plateau.h"
 
+#define LENGTH 1000
+
 /* implémentation du type case */
-struct case {
+struct Case {
     carte carte; // carte posée sur la case en question
     int id_faction; // 0 ou 1 qui détermine la faction qui a posée la carte
     int etat; // 0 si la carte est face cachée ou 1 si elle est face visible
@@ -9,22 +11,35 @@ struct case {
 
 /* implémentation du type plateau */
 struct plateau {
-    case tab[1000][1000]; // matrice de cases de taille 1000 x 1000
-<<<<<<< HEAD
+    Case** tab; // matrice de cases de taille 1000 x 1000
 };
-=======
-} plateau;
+
+struct faction {
+    char nom;
+    int nombre_points_DDRS;
+    pile main; /* type liste défini dans structure.h */
+    pile pioche; /* type pile défini dans structure.h */
+};
 
 plateau init_plateau(){
-
+    plateau _plateau;
+    _plateau.tab = (Case**) malloc(LENGTH * sizeof(Case*));
+    for(int i = 0; i < LENGTH; i++){
+        _plateau.tab[i] = (Case*) malloc(LENGTH * sizeof(Case));
+    }
+    return _plateau;
 }
 
-void libere_plateau(plateau p){
-
+void libere_plateau(plateau _plateau){
+    for(int i = 0; i < LENGTH; i++){
+        free(_plateau.tab[i]);
+    }
+    free(_plateau.tab);
 }
 
-int fin_manche(plateau p){
-
+int init_manche(plateau p){
+    //tire carte
+    // 1 si fin 0 sinon
 }
 
 faction* liste_faction(){
@@ -38,4 +53,3 @@ void poser(plateau p, carte c, int* pos){
 carte retourner(plateau p, faction* facs){
 
 }
->>>>>>> 2af9bccde026a905907e5a64b94c2e40e23a75f5

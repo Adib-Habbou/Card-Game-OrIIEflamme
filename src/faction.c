d61be609d6808827b94ef45669e05f3089d3836b
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-03-22 10:10:12 +0100

    Tâche B.5 - implémentation des getters de faction

--- src/faction.c
+++ src/faction.c
@@ -7,3 +7,31 @@ struct faction {
     pile main; /* type liste défini dans structure.h et structure.c */
     pile pioche; /* type pile défini dans structure.h et structure.c */
 };
+
+/*  @requires   une faction valide
+    @assigns    rien
+    @ensures    renvoie le nom de la faction */
+char get_faction_nom(faction faction) {
+    return faction.nom;
+}
+
+/*  @requires   une faction valide
+    @assigns    rien
+    @ensures    renvoie le nombre de points DDRS de la faction */
+int get_faction_nombre_points_DDRS(faction faction) {
+    return faction.nombre_points_DDRS;
+}
+
+/*  @requires   une faction valide
+    @assigns    rien
+    @ensures    renvoie la main de la faction */
+pile get_faction_main(faction faction) {
+    return faction.main;
+}
+
+/*  @requires   une faction valide
+    @assigns    rien
+    @ensures    renvoie la pioche de la faction */
+pile get_faction_pioche(faction faction) {
+    return faction.pioche;
+}

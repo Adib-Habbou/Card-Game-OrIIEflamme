4062099de0d872eb65066ebd29ec81490d3d7d37
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-03-22 10:16:06 +0100

    Tâche B.5 - setter de carte.h

--- headers/carte.h
+++ headers/carte.h
@@ -16,7 +16,22 @@ char get_carte_description(carte carte);
 
 /*  @requires   une carte valide
     @assigns    rien
-    @ensures    renvoie le nomnre d'occurrences de la carte */
+    @ensures    renvoie le nombre d'occurrences de la carte */
 int get_carte_nombre_occurrences(carte carte);
+
+/*  @requires   une carte valide et une chaîne de caractères
+    @assigns    carte
+    @ensures    modifie le nom de la carte */
+void set_carte_nom(carte carte, char nom);
+
+/*  @requires   une carte valide et une chaîne de caractères
+    @assigns    carte
+    @ensures    modifie la description de la carte */
+void set_carte_description(carte carte, char description);
+
+/*  @requires   une carte valide et un entier
+    @assigns    carte
+    @ensures    modifie le nomnre d'occurrences de la carte */
+void set_carte_nombre_occurrences(carte carte, int nombre_occurrences);
 
 #endif

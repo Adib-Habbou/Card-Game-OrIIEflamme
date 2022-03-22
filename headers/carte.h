91cd9b776efb8a7f29781772a1edaba02e4a399f
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-03-22 10:05:21 +0100

    Tâche B.5 - fonction getter dans carte faction et plateau - version 1

--- headers/carte.h
+++ headers/carte.h
@@ -4,4 +4,19 @@
 /* définition du type abstrait des cartes */
 typedef struct carte *carte;
 
+/*  @requires   une carte valide
+    @assigns    rien
+    @ensures    renvoie le nom de la carte */
+char get_carte_nom(carte carte);
+
+/*  @requires   une carte valide
+    @assigns    rien
+    @ensures    renvoie la description de la carte */
+char get_carte_description(carte carte);
+
+/*  @requires   une carte valide
+    @assigns    rien
+    @ensures    renvoie le nomnre d'occurrences de la carte */
+int get_carte_nombre_occurrences(carte carte);
+
 #endif

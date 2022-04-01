40013e6053b1f3e22cf0dbb7aeef7aab52fab9ee
Willy KINFOUSSIA <willy.kinfoussia@pc130-22.pedago.ensiie.fr>
2022-04-01 14:25:52 +0200

    Dependance du main.c

--- headers/main.h
+++ headers/main.h
@@ -1,14 +1,18 @@
 /* importation du module carte */
 #include "carte.h"
+#include "../src/carte.c"
 
 /* importation du module faction */
 #include "faction.h"
+#include "../src/faction.c"
 
 /* importation du module structure */
 #include "interface.h"
+#include "../src/interface.c"
 
 /* importation du module plateau */
 #include "plateau.h"
+#include "../src/plateau.c"
 
 /* Pour le null */
 #include <stddef.h>

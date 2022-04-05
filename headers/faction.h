8f25f042d46c580665c319f4005690cfbf5110a5
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-04-05 11:19:46 +0200

    Tâche B.5 modification des arguments de set_faction_pioche

--- headers/faction.h
+++ headers/faction.h
@@ -123,7 +123,7 @@ void set_faction_main(faction faction, pile main);
 /*  @requires   une faction valide et une pile
     @assigns    faction
     @ensures    modifie la pioche de la faction */
-void set_faction_pioche(faction faction, pile pioche);
+void set_faction_pioche(faction faction, carte* pioche);
 
 /*  @requires   une faction valide et un entier
     @assigns    faction

2edb5f2770500a2cb83da53c3dde00d3da2bee1c
Clark <clark@Clark_pc.localdomain>
2022-04-25 16:54:46 +0200

    debug

--- src/main.c
+++ src/main.c
@@ -44,7 +44,7 @@ int main(){
             // Retourne la carte choisi par la faction
             carte _carte = carte_choisie(_factions[i%2]);
             // Retourne la position sur le plateau où la faction décide de poser la carte
-            int* _position = carte_positon(_plateau); 
+            int* _position = carte_positon(_plateau,i%2); 
             //Pose la carte sur le plateau
             poser(_plateau, _carte, _position);
             affiche_plateau(_plateau);

7cd6ce88308fd90633a005ed4cc9675176215a10
Adib Habbou <adib@LAPTOP-MKI2VF0P.localdomain>
2022-04-25 16:32:18 +0200

    willy arrête de troll lit les commentaires

--- src/plateau.c
+++ src/plateau.c
@@ -17,7 +17,7 @@ struct plateau {
 /*  @requires   une case valide
     @assigns    rien
     @ensures    renvoie la carte posé sur la case */
-carte get_case_carte(Case _case) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CA NE MARCHE PAS SEG FAULT !!!!!!!!!!!!!!!!!!!!!!!!!!
+carte get_case_carte(Case _case) {
     return _case->carte;
 }
 

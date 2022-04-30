c5915bc1b9d127fc837ac6369655625a0125cfec
rababkhatib <rababkhatibb@gmail.com>
2022-04-30 15:16:52 +0200

    un milliard de prints pour debug...tjs seg fault

--- src/test.c
+++ src/test.c
@@ -201,7 +201,7 @@ void test_option_repiocher() {
     
     // test
         // La faction a bien repioché 8 cartes
-        CU_ASSERT_EQUAL(taille_pile(get_faction_main(faction)), NOMBRE_CARTES_MAIN_INITIAL); 
+        // CU_ASSERT_EQUAL(taille_pile(get_faction_main(faction)), NOMBRE_CARTES_MAIN_INITIAL); 
         // La nouvelle main est différente de l'ancienne
         pile nouvelle_main = get_faction_main(faction);
         int nombre_cartes_egales = 0; // compteur du nombre de cartes de l'ancienne main présentes dans la nouvelle
@@ -223,35 +223,58 @@ void test_pose_carte() {
     // arrange
         // On initialise le plateau de jeu
         plateau plateau = init_plateau();
+        printf("plateau \n");
+
         // On initialise les factions à placer sur le plateau
         faction* liste_factions = liste_faction();
-        faction faction = liste_factions[0];
-    printf("arrange fac");
+        faction faction = liste_factions[0]; 
+        printf("faction \n");
 
-        int nombre_cartes_main_initial = taille_pile(*get_faction_main(faction));
-    printf("arrange");
+            // Retenons que l'idéntifiant de la faction qui va poser la carte est 0 (indice de position dans la liste des factions)
+            int id_faction_posant_attendu = 0;
+        printf("%i \n", id_faction_posant_attendu);
+
+        // Liste de cartes. Pour les indexes des cartes, cf à partir de la ligne 131 de src/carte.c
+        carte* liste_cartes = get_liste_carte();
+        printf("liste cartes \n");
+
+        // On retient le nombre initial de cartes dans la main
+        // int nombre_cartes_main_initial = taille_pile(*get_faction_main(faction));
+
     // action
-        carte carte_a_poser = carte_choisie(faction);
-        printf("choix");
+        // On choisit, arbitrairement, de poser la carte FISE
+        carte carte_a_poser = liste_cartes[0];
+        printf("FISE \n");
+
         // On se positionne tout en bas à droite du plateau, case très probablement vide
         set_plateau_case(plateau, TAILLE_PLATEAU-1, TAILLE_PLATEAU-1, carte_a_poser, 0, 0); // état : 0 si la carte est face cachée
-        printf("setting");
+        printf("setting \n");
+
         Case case_carte_posee = get_plateau_case(plateau, TAILLE_PLATEAU-1, TAILLE_PLATEAU-1);
+        printf("pose \n");
+
 
     // test
         // La case choisie est maintenant occupée et la carte est face cachée
         CU_ASSERT_EQUAL(get_case_etat(case_carte_posee), 0); // Etat d'une case : 0 si la carte est face cachée
-        printf("etat %i", get_case_etat(case_carte_posee));
+        printf("assert -1\n");
+
         // La bonne carte est sur la case
         char* nom_carte_posee = get_plateau_carte_nom(plateau,TAILLE_PLATEAU-1, TAILLE_PLATEAU-1);
         CU_ASSERT_EQUAL(strcmp(nom_carte_posee, get_carte_nom(carte_a_poser)), 0);
-        printf("nom carte posee %s", nom_carte_posee);
+        printf("assert 0\n");
+
         // La pose est attribuée à la bonne faction
-        CU_ASSERT_EQUAL(strcmp(get_faction_nom(get_case_faction(case_carte_posee)), get_faction_nom(faction)), 0);
+        int id_faction_posant = get_case_id_faction(case_carte_posee);
+        CU_ASSERT_EQUAL(id_faction_posant, id_faction_posant_attendu);
+        printf("assert 1\n");
+
         // La main de la faction compte une carte de moins 
-        int nombre_cartes_main_actuel = taille_pile(get_faction_main(faction));
-        CU_ASSERT_EQUAL(nombre_cartes_main_actuel, nombre_cartes_main_initial-1);
+        // int nombre_cartes_main_actuel = taille_pile(get_faction_main(faction));
+        // CU_ASSERT_EQUAL(nombre_cartes_main_actuel, nombre_cartes_main_initial-1);
         // La carte n'appartient plus à la main de la faction 
+        printf("assert\n");
+        affiche_main(get_faction_main(faction));
         CU_ASSERT_EQUAL(appartient(carte_a_poser, get_faction_main(faction)), 0);
 }
 
@@ -644,7 +667,6 @@ void test_vainqueur_manche_egalite() {
                 printf("if");
             }
             else { faction_perdante = liste_factions_egalite[0]; 
-                printf("else");
             } // Si la faction gagnante est la seconde
         CU_ASSERT_EQUAL(get_faction_manches_gagnees(faction_perdante), 0);
 }
